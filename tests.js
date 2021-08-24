865de8bdca090b7518ca45597fd328159c2bdabc
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-08-24 11:43:36 -0500

    Refactored code for intro-to-testing

--- tests.js
+++ tests.js
@@ -24,7 +24,7 @@ describe('sayHello', function() {
     });
 
     it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
 
     it('should return the string "Hello, Alex!" when executed', function() {
